57d896a89e18ddb8518b310557b00c77ce6ec6c9
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2024-12-14 01:21:28 +0300

    Update libogg and libvorbis third party modules to versions 1.3.5 and 1.3.7, respectively.

--- Sources/ThirdParty/README.txt
+++ Sources/ThirdParty/README.txt
@@ -4,7 +4,7 @@ Third party modules used by Serious Engine.
 Current module versions:
 
 glew - 2.1.0
-libogg - whichever one was included in official 1.10 release
-libvorbis - whichever one was included in official 1.10 release
+libogg - 1.3.5
+libvorbis - 1.3.7
 SDL2 - 2.28.3
 zlib - whichever one was initially used within Engine in official 1.10 release
--- Sources/ThirdParty/libogg/include/ogg/config_types.h
+++ Sources/ThirdParty/libogg/include/ogg/config_types.h
@@ -3,7 +3,7 @@
 
 /* [Cecil] Manually configured for Linux from "config_types.h.in" */
 
-/* these are filled in by configure */
+/* these are filled in by configure or cmake*/
 #define INCLUDE_INTTYPES_H 0
 #define INCLUDE_STDINT_H 1
 #define INCLUDE_SYS_TYPES_H 0
@@ -23,5 +23,6 @@ typedef uint16_t ogg_uint16_t;
 typedef int32_t  ogg_int32_t;
 typedef uint32_t ogg_uint32_t;
 typedef int64_t  ogg_int64_t;
+typedef uint64_t ogg_uint64_t;
 
 #endif
