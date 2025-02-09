e54fe05c6e96380672c8133072ce534cbbf9f8e3
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2025-02-09 15:58:46 +0300

    Fix SE_SetReportLogFileName() not setting an absolute path inside.

--- Sources/Engine/Base/StackDump.cpp
+++ Sources/Engine/Base/StackDump.cpp
@@ -70,7 +70,7 @@ extern void SE_SetReportLogFileName(const CTString &fnmReport) {
   // Create directories for the report file
   CreateAllDirectories(fnmReport);
 
-  g_MSJExceptionHandler.SetLogFileName(fnmReport.ConstData());
+  g_MSJExceptionHandler.SetLogFileName((_fnmApplicationPath + fnmReport).ConstData());
 };
 
 //============================== Global Variables =============================
